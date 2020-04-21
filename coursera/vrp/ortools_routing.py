36adce85fafc575a3ec7af2a25075556d8f9f0d3
Đặng Xuân Vương <vuong.1998@gmail.com>
2020-04-21 07:46:45 +0700

    update vrp solver using or-tools routing model

--- coursera/vrp/ortools_routing.py
+++ coursera/vrp/ortools_routing.py
@@ -2,34 +2,7 @@ from __future__ import print_function
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 from utils import *
-
-def print_solution(data, manager, routing, solution):
-	"""Prints solution on console."""
-	total_distance = 0
-	total_load = 0
-	for vehicle_id in range(data['num_vehicles']):
-		index = routing.Start(vehicle_id)
-		plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
-		route_distance = 0
-		route_load = 0
-		while not routing.IsEnd(index):
-			node_index = manager.IndexToNode(index)
-			route_load += data['demands'][node_index]
-			plan_output += ' {0} Load({1}) -> '.format(node_index, route_load)
-			previous_index = index
-			index = solution.Value(routing.NextVar(index))
-			route_distance += routing.GetArcCostForVehicle(
-				previous_index, index, vehicle_id)
-		plan_output += ' {0} Load({1})\n'.format(manager.IndexToNode(index),
-												 route_load)
-		plan_output += 'Distance of the route: {}m\n'.format(route_distance)
-		plan_output += 'Load of the route: {}\n'.format(route_load)
-		print(plan_output)
-		total_distance += route_distance
-		total_load += route_load
-	print('Total distance of all routes: {}m'.format(total_distance))
-	print('Total load of all routes: {}'.format(total_load))
-
+from naive import *
 
 def ortools_routing(customers, depot, customer_count, vehicle_count, vehicle_capacity):
 
@@ -59,7 +32,7 @@ def ortools_routing(customers, depot, customer_count, vehicle_count, vehicle_cap
 		"""Returns the demand of the node."""
 		# Convert from routing variable Index to demands NodeIndex.
 		from_node = manager.IndexToNode(from_index)
-		return customers[from_index].demand
+		return customers[from_node].demand
 
 	demand_callback_index = routing.RegisterUnaryTransitCallback(demand_callback)
 	routing.AddDimensionWithVehicleCapacity(
@@ -73,13 +46,47 @@ def ortools_routing(customers, depot, customer_count, vehicle_count, vehicle_cap
 	search_parameters = pywrapcp.DefaultRoutingSearchParameters()
 	search_parameters.first_solution_strategy = (
 		routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+	search_parameters.local_search_metaheuristic = (
+		routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
+	search_parameters.time_limit.seconds = 6
+	search_parameters.log_search = True
 
 	# Solve the problem.
 	solution = routing.SolveWithParameters(search_parameters)
 
 	# Print solution on console.
 	if solution:
-		print_solution(data, manager, routing, solution)
+		all_routes = []
+		total_distance = 0
+		total_load = 0
+		for vehicle_id in range(vehicle_count):
+			index = routing.Start(vehicle_id)
+			plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+			route = []
+			route_distance = 0
+			route_load = 0
+			while not routing.IsEnd(index):
+				node_index = manager.IndexToNode(index)
+				route.append(customers[node_index])
+				route_load += customers[node_index].demand
+				plan_output += '{0} Load({1}) -> '.format(node_index, route_load)
+				previous_index = index
+				index = solution.Value(routing.NextVar(index))
+				route_distance += routing.GetArcCostForVehicle(previous_index, index, vehicle_id)
+				plan_output += ' {0} Load({1})\n'.format(manager.IndexToNode(index), route_load)
+				plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+				plan_output += 'Load of the route: {}\n'.format(route_load)
+				print(plan_output)
+				total_distance += route_distance
+				total_load += route_load
+			route.append(customers[0])
+			all_routes.append(route[1:-1])
+		print('Total distance of all routes: {}m'.format(total_distance))
+		print('Total load of all routes: {}'.format(total_load))
+		print(all_routes)
+		return all_routes, 0
+	else:
+		return naive(customers, depot, customer_count, vehicle_count, vehicle_capacity)
 
 
 if __name__ == '__main__':
