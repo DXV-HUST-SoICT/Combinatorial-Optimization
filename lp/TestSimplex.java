7ac1503881e583852f415946c03a7d08a4eeee8c
Đặng Xuân Vương <vuong.1998@gmail.com>
2020-05-06 07:30:50 +0700

    remove class file

--- lp/TestSimplex.java
+++ lp/TestSimplex.java
@@ -1,8 +1,17 @@
 public class TestSimplex {
 	public static void main(String[] args) {
-		double[][] a = new double[][] { {5, 15}, {4, 4}, {35, 20} };
-		double[] b = new double[] {480, 160, 1190};
-		double[] c = new double[] {13, 23};
+		// double[][] a = new double[][] { {5, 15}, {4, 4}, {35, 20} };
+		// double[] b = new double[] {480, 160, 1190};
+		// double[] c = new double[] {13, 23};
+		// double[][] a = new double[][] {{6, 0, 1, -1, 0}, {0, 4, 1, 1, 0}, {0, 0, -1, -1, 4}};
+  //       double[] b = new double[] {6, 6, -2};
+  //       double[] c = new double[] {0, 1, 0, 0, 0};
+		// double[][] a = new double[][] {{3, 2}, {-3, 2}};
+  //       double[] b = new double[] {6, 0};
+  //       double[] c = new double[] {0, 1};
+		double[][] a = new double[][] {{6, 0, 1, -1, 0}, {0, 4, 1, 1, 0}, {0, 1, 0, 0, 0}};
+        double[] b = new double[] {6, 6, 1};
+        double[] c = new double[] {0, 1, 0, 0, 0};
 		Simplex s = new Simplex(a, b, c);
 		s.solve();
 		int m = s.getM();
